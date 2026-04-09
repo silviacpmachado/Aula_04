39c956573a8113930668b03ec4f2ec72d326e481
silviacpmachado-sketch <silviacpmachado@gmail.com>
2026-04-09 19:09:31 -0300

    inicializa o repositorio

--- aula04/exemplo01.py
+++ aula04/exemplo01.py
@@ -1,31 +1,73 @@
-# idade = 16
-# if idade >=18:
-#     print('você é adulto')
+# # idade = 16
+# # if idade >=18:
+# #     print('você é adulto')
+# # else:
+# #     print('Você não é adulto')
+
+# # #--------------------------------
+
+# # pontos = int(input('Informe os pontos: '))
+# # if pontos >= 100:
+# #     print('excelente')
+# # elif pontos >= 50:
+# #     print('Bom Desempenho')
+# # elif pontos >= 25:
+# #     print('Satisfatório')
+# # else:
+# #     print('Pratique Mais...')
+
+# # Operadores AND e OR
+# usuario = input('Nome: ')
+# senha = input('Senha: ')
+
+# if usuario == "admin" and senha == "1234":
+#     print('Login Realizado com Sucesso')
 # else:
-#     print('Você não é adulto')
+#     print('Usuário ou senha incorretos')
 
-# #--------------------------------
-
-# pontos = int(input('Informe os pontos: '))
-# if pontos >= 100:
-#     print('excelente')
-# elif pontos >= 50:
-#     print('Bom Desempenho')
-# elif pontos >= 25:
-#     print('Satisfatório')
+# if usuario == "admin" or senha == "1234":
+#     print('Login Realizado com Sucesso')
 # else:
-#     print('Pratique Mais...')
+#     print('Usuário ou senha incorretos')
 
-# Operadores AND e OR
-usuario = input('Nome: ')
-senha = input('Senha: ')
-
-if usuario == "admin" and senha == "1234":
-    print('Login Realizado com Sucesso')
+#Exemplo IFs encadeados - verifica todas
+nota = 5.5
+if nota >= 9:
+    print('A')
+elif nota >= 7:
+    print('B')
+elif nota >= 5:
+    print('C')
+elif nota >= 3:
+    print('D')
 else:
-    print('Usuário ou senha incorretos')
+    print('E')
 
-if usuario == "admin" or senha == "1234":
-    print('Login Realizado com Sucesso')
+#Exemplo IFs não encadeados
+nota = 10
+if nota >= 9:
+    print('A')
+
+if nota >= 7:
+    print('B')
+
+if nota >= 5:
+    print('C')
+
+elif nota >= 3:
+    print('D')
 else:
-    print('Usuário ou senha incorretos')
+    print('E')
+
+#Ifs aninhados
+nota = float(input('Insira a nota: '))
+frequencia = float(input('Informe a frequencia'))
+
+if nota >=7:
+    #Aprovado por nota, mas precisa checar a frequência.
+    if frequencia >= 75:
+        print('Aluno aprovado por Nota e Frequência')
+    else:
+        print('Reprovado por frequência baixa')
+else: 
+    print('Reprovado por nota baixa.')
